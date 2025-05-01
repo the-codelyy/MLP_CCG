e7b7bf92126ef3519861c1643c7252c521b65ddf
Adam <codelyoko3131@gmail.com>
2025-05-01 01:29:01 +0100

    Base Card Design

---
+++ Assets/Scripts/Data/CardData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Base Card", menuName = "Cards/Base Card")]
+public class CardData : ScriptableObject
+{
+    public string Name;
+    public string Tagline;
+}
